d2f2630b26b336aba506386ef601d5cf2ec7f431
wonny-log <wonny727@gmail.com>
2020-07-11 15:24:18 +0900

    연산을 수행하여도 기존 amount 값이 변경되지 않도록 로직 수정

--- Dollar.test.ts
+++ Dollar.test.ts
@@ -1,9 +1,17 @@
 import Dollar from "./Dollar";
 
 describe("testMultiplication", () => {
-  test("1. 다중 통화를 지원하는 Money 객체", () => {
+  xtest("1. 다중 통화를 지원하는 Money 객체", () => {
     const five: Dollar = new Dollar(5);
     five.times(2);
     expect(five.amount).toBe(10);
+  });
+
+  test("2. 타락한 객체", () => {
+    const five: Dollar = new Dollar(5);
+    let product: Dollar = five.times(2);
+    expect(product.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });
